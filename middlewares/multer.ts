3cbf8cb1220bcfc85d204b9dbf4f14e7c5f85824
Yash Manek <yashmanek2001@gmail.com>
2025-02-11 20:34:28 +0530

    Added cloudinary

---
+++ middlewares/multer.ts
@@ -0,0 +1,37 @@
+import multer, { FileFilterCallback } from "multer";
+import path from "path";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary";
+import { Request } from "express";
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: async (req: Request, file: Express.Multer.File) => {
+    const format = file.mimetype.replace("image/", ""); 
+    if (!["jpg", "jpeg", "png", "gif"].includes(format)) {
+      throw new Error("Unsupported file format");
+    }
+    return {
+      folder: "playpals",
+      format, 
+      public_id: `${Date.now()}-${path.parse(file.originalname).name.replace(/\s+/g, "_")}`,
+    };
+  },
+});
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 7 * 1024 * 1024, 
+  },
+  fileFilter: (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
+    const allowedMimeTypes = ["image/jpeg", "image/png", "image/gif"];
+    if (allowedMimeTypes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Invalid file type"));
+    }
+  },
+});
+
+export default upload;
--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,12 +1,9 @@
-import { Router } from 'express';
-import {
-  createUser,
-  getUser,
-  getUsers,
-  
-} from '../controllers/user.controller';
+import { Router } from "express";
+import { createUser, getUser, getUsers } from "../controllers/user.controller";
+import upload from "../middlewares/multer";
+
 const userRoute = Router();
-userRoute.post('', createUser);
-userRoute.get('', getUsers);
-userRoute.get('/:userid', getUser);
+userRoute.post("/create", upload.single("profilePhoto"), createUser);
+userRoute.get("", getUsers);
+userRoute.get("/:userid", getUser);
 export default userRoute;
